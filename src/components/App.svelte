6e946b7c910cafb1654aaeea6c718b8bbfba40e2
Matt Daniels <mattdaniels@google.com>
2021-08-06 16:25:29 -0400

    here

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -12,26 +12,79 @@
   import Footer from "./pudding/Footer.svelte";
   import copy from "../data/doc.json";
   import mq from "../stores/mq.js";
-  import {selectAll} from 'd3-selection';
+  import {selectAll, select} from 'd3-selection';
   import { onMount } from "svelte";
 
   let mounted;
 
+  let suit = {
+    0: "♠",
+    1: "♥",
+    2: "♦",
+    3: "♣"
+  }
+
+  let suitColor = {
+    0: "black",
+    1: "red",
+    2: "red",
+    3: "black"
+  }
+
+  let cardOrder = {
+    0:2,
+    1:3,
+    2:4,
+    3:5,
+    4:6,
+    5:7,
+    6:8,
+    7:9,
+    8:10,
+    9:"J",
+    10:"Q",
+    11:"K",
+    12:"A"
+  }
+
+  function convertRemToPixels(rem) {    
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+  }
+
 
   import { Swiper, SwiperSlide } from 'swiper/svelte';
   import SwiperCore, {
-    Mousewheel,Pagination
+    Controller,Mousewheel,Pagination
   } from 'swiper/core';
 
   // install Swiper modules
-  SwiperCore.use([Mousewheel,Pagination]);
+  SwiperCore.use([Mousewheel,Pagination,Controller]);
   
+  function pivotPopOff(e){
+    let el = select(e.srcElement);
+    if(!el.classed("pop-off-centered")){
+      el.classed("pop-off-centered",true);
+    }
+    else {
+      el.classed("pop-off-centered",false);
+    }
+
+  }
+
+  // let controlledSwiper = null;
+  const onSwiper = (e) => {
+      const [swiper] = e.detail;
+      console.log(swiper);
+      swiper.slideTo(4);
+  }
+
   onMount(() => {
     mounted = true;
 
-    selectAll("a").attr("target","_blank").each(d => {
-      console.log("here");
-    });
+    console.log("mounted")
+
+    select("#content").selectAll("a").attr("target","_blank")
+
   });
 
   
@@ -46,18 +99,49 @@
 
 <!-- svelte-ignore missing-declaration -->
 {#if mounted}
-  <section>
+  <section class="card-container">
     <Swiper
-      direction="{'vertical'}" slidesPerView="{1}" spaceBetween="{30}" mousewheel="{true}"
+      direction="{'vertical'}" slidesPerView="{'auto'}" spaceBetween="{convertRemToPixels(.5)}" mousewheel="{{forceToAxis:true}}"
       on:slideChange={() => console.log('slide change')}
-      on:swiper={(e) => console.log(e.detail[0])}
+      on:swiper={onSwiper}
     >
 
-      {#each copy.cards as card}
-        <SwiperSlide>
-          {#each card.cardText as cardText}
-            <p>{@html cardText.value}</p>
-          {/each}
+      {#each copy.cards as card, index}
+
+        {#if card.background}
+          <div class=pop-off-wrapper>
+            <div class="pop-off card-slide" on:click={pivotPopOff}>
+              <div class="text-wrapper">
+                <p>{@html card.background}</p>
+              </div>
+            </div>
+          </div>
+          
+        {/if}
+        <SwiperSlide class="card-slide">
+          <div class="card-wrapper">
+
+            <div class="card-symbol card-color-{suitColor[Math.floor(index/13)]}">
+              <p class="card-num">{ cardOrder[(index) % 13] }</p>
+              <p class="card-suit">{ suit[Math.floor(index/13)] }</p>
+            </div>
+
+            {#if card.cardTitle}
+              <p class="card-title card-title-hidden">{ card.cardTitle }</p>
+            {/if}
+            {#if card.img}
+              <img class="{card.imgSize}" src="assets/{card.img}" alt={card.altText}>
+            {/if}
+
+            <div class="text-wrapper">
+              {#each card.cardText as cardText}
+                <!-- <p>{@index}</p> -->
+                <p>{@html cardText.value}</p>
+              {/each}
+            </div>
+          </div>
+          
+          
         </SwiperSlide>
       {/each}
     </Swiper>
@@ -65,84 +149,121 @@
   </section>
 {/if}
 
-
-
-<section>
-</section>
-
-
-
-<!-- <section>
-  <Prose grafs="{copy.popularityBefore}" />
-  <FigureInfo hed="{copy.popularityHed}" />
-  <div class="swarm swarm-horizontal">
-    <Swarm bind:downloadData="{downloadSwarmData}" />
-  </div>
-  <div class="swarm swarm-vertical">
-    <Swarm orientation="{'vertical'}" bind:downloadData="{downloadSwarmData}" />
-  </div>
-  <FigureSource
-    source="{copy.popularitySource}"
-    data="{downloadSwarmData}"
-    filename="solo-artist-followers.csv"
-  />
-  <Prose grafs="{copy.popularityAfter}" />
-</section> -->
-
-<!-- <section bind:this="{scatterEl}">
-  {#if mobileScatter || selectedScatter === "table"}
-    <FigureInfo hed="{copy.successHed}" />
-  {:else}
-    <FigureInfo hed="{copy.successHedBig}">
-      <select
-        aria-label="Band"
-        bind:value="{scatterActiveBand}"
-        class:selected="{!!scatterActiveBand}"
-      >
-        <option value="">all bands</option>
-        {#each scatterBands as names}
-          <option value="{names[0].name}">{shorten(names[0].name)}</option>
-        {/each}
-      </select>
-      <span>{@html membersText}</span>
-    </FigureInfo>
-  {/if}
-  <div class="scatter-view">
-    <ButtonSet
-      options="{[{ value: 'chart' }, { value: 'table' }]}"
-      legend="View as"
-      legendPosition="{isSmall ? 'left' : 'top'}"
-      bind:selected="{selectedScatter}"
-    />
-  </div>
-  <Scatter
-    bind:scatterBands
-    activeBand="{scatterActiveBand}"
-    bind:downloadData="{downloadScatterData}"
-    showTable="{selectedScatter === 'table'}"
-  />
-  <FigureSource
-    source="{copy.successSource}"
-    data="{downloadScatterData}"
-    filename="billboard-hits.csv"
-  />
-  <Prose grafs="{copy.successAfter}" />
-</section>
-
-<section id="method">
-  <Prose className="small" grafs="{copy.method}" />
-</section> -->
-
-<!-- <Footer /> -->
-
 <style>
   section {
     margin: 0 auto;
-    padding: 0 1em;
   }
 
+
   p {
-    font-size:16px;
+    font-size:1rem;
   }
+
+  .card-container {
+    background-color: var(--bg);
+  }
+
+  .card-title-hidden {
+    display: none;
+  }
+
+  .pop-off-wrapper {
+    height: 1px;
+    position:relative;
+    z-index: 100;
+  }
+
+  .pop-off {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100%;
+    z-index: 1000;
+    transform: translate(calc(-100% + 120px), calc(0% - 150px)) rotate(25deg);
+    transition: transform 0.3s; 
+  }
+
+  .pop-off:before {
+    content: '';
+    width: 42px;
+    height: 43px;
+    position: absolute;
+    top: -6px;
+    right: -25px;
+    background: url(assets/hand.png);
+    background-repeat: no-repeat;
+    background-size: contain;
+    transform: rotate(-17deg);
+  }
+
+  .pop-off .text-wrapper {
+    pointer-events: none;
+  }
+
+  .pop-off .text-wrapper a{
+    pointer-events: all;
+  }
+
+  .pop-off-centered {
+    transform: translate(0, calc(-100vh + 3rem)) rotate(0deg);
+  }
+
+  .pop-off-centered:before {
+    display: none;
+  }
+
+  .card-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .card-symbol {
+    position: absolute;
+    top: 1rem;
+    left: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 36px;
+  }
+
+  .text-wrapper {
+    width: calc(100% - 2rem);
+    margin: 0 auto;
+  }
+
+  .card-color-black {
+    color: var(--black);
+  }
+
+  .card-color-red {
+    color: var(--red);
+  }
+
+
+
+  .card-num, .card-suit {
+    font-size: 1.7rem;
+    margin: 0;
+    line-height: .9;
+    text-align: center;
+  }
+
+  .card-suit {
+    font-size: 2rem;
+  }
+
+  .full {
+    width: 100%;
+  }
+
+  .half {
+    width: 100%;
+  }
+
+  
 
 </style>
